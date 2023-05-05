2e74d19a69f448844a8baad39266e0d6043ed881
tronghieu88 <ngthieu88@gmail.com>
2023-05-06 04:27:37 +0700

    dist

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const fs = require('fs');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const dir = './tmp';
+  const dir = '../tmp';
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
